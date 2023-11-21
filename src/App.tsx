9858b3299577b1c5b24baf3ec140382c0d860eff
Elmar Ismayilov <bamazy@gmail.com>
2023-11-21 11:17:15 +0800

    add vite settings

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
   const [data, setData] = useState([12, 5, 6, 6, 9, 10]);
   const [width, setWidth] = useState(1000);
   const [height, setHeight] = useState(500);
-  const id = document.getElementById('root') 
+  const id = document.getElementById("root");
 
   return (
     <div className="App">
-      <BarChart data={data} width={width} height={height} />
+      <BarChart data={data} width={width} height={height} id={id} />
     </div>
   );
 }
